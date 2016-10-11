cc47b3547c7342ac33af115a134e45fb1992d93f
Alexsander <alexs.dev@websm.io>
2016-10-11 15:31:33 +0300

    add event model, change position property to transform

--- src/scroller.js
+++ src/scroller.js
@@ -157,37 +157,3 @@ Scroller.prototype.on = function (name, handler) {
 	this._events[name].push(handler);
 
 }
-
-;"use strict";
-
-(function () {
-
-	var EventEmitter = function () {
-
-		this._queue = [];
-
-	}
-
-	EventEmitter.prototype.dispatch = function () {
-
-		for(var i = 0; i != this._queue.length; i++)
-			this._queue[i].apply(this, arguments);
-
-	}
-
-	EventEmitter.prototype.push = function (handler) {
-
-		this._queue.push(handler);
-		return this;
-
-	}
-
-	EventEmitter.prototype.clear = function () {
-
-		this._queue = [];
-
-	}
-
-	Scroller.prototype.EventEmitter = EventEmitter;
-
-})();
